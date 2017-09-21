0c5736e7ea0e9cc3e467eca3cd3d9daabd4cd96b
Javi M Valle <javi.mart.valle@gmail.com>
2017-09-21 19:07:39 +0200

    no message

---
+++ Assets/GameAssets/_Scripts/Player/ArcherAnimatorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent((typeof(Animator)))]
+public class ArcherAnimatorController : MonoBehaviour
+{
+    private const string SPEED_KEY = "Speed";
+    private const string EQUIP_KEY = "Equip";
+    private const string EQUIPED_KEY = "Equiped";
+    private const string DISARM_KEY = "Disarm";
+    private const string SHOOT_KEY = "Shoot";
+
+    private Animator _animator;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetSpeed(float speed)
+    {
+        _animator.SetFloat(SPEED_KEY, speed);
+    }
+
+    public void SetEquip()
+    {
+        _animator.SetTrigger(EQUIP_KEY);
+        _animator.SetBool(EQUIPED_KEY, true);
+    }
+
+    public void SetDisarm()
+    {
+        _animator.SetTrigger(DISARM_KEY);
+        _animator.SetBool(EQUIPED_KEY, false);
+    }
+
+    public void SetShoot(bool shoot)
+    {
+        _animator.SetBool(SHOOT_KEY, shoot);
+    }
+
+    public void SetCrouch(bool crouching)
+    {
+        _animator.SetLayerWeight(1, crouching ? 1 : 0);
+    }
+}
---
+++ Assets/GameAssets/_Scripts/Player/ArcherController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(ArcherAnimatorController))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class ArcherController : MonoBehaviour
+{
+    private ArcherAnimatorController _archerAnimator;
+    private NavMeshAgent _navMeshAgent;
+
+    private bool _bCrouching;
+
+    void Awake()
+    {
+        _archerAnimator = GetComponent<ArcherAnimatorController>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _bCrouching = false;
+    }
+
+    void ScreenRaycast()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            if (hit.transform.CompareTag("Walkable"))
+            {
+                Move(hit.point);
+            }
+        }
+    }
+
+    void Move(Vector3 destination)
+    {
+        _navMeshAgent.SetDestination(destination);
+        _archerAnimator.SetSpeed(_navMeshAgent.speed);
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _bCrouching = !_bCrouching;
+            _archerAnimator.SetCrouch(_bCrouching);
+        }
+    }
+
+}
